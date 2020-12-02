bed7689fabd5ac4923f8909f92157edf9407db81
Pierre Jacquet <pierre.jacquet@etu.u-pec.fr>
2020-12-02 13:48:06 +0100

    first commit

---
+++ src/main/java/com/episen/ing3/tpmra/TpmraApplication.java
@@ -0,0 +1,13 @@
+package com.episen.ing3.tpmra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpmraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpmraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/episen/ing3/tpmra/TpmraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.episen.ing3.tpmra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpmraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
