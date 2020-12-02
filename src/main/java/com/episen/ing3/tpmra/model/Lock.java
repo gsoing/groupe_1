7f5205cd248c4934d2f17c6e6e5c6aa383a3c3b4
Justin Aguesse <justin.aguesse@etu.u-pec.fr>
2020-12-02 16:43:26 +0100

    Update Lock.java

--- src/main/java/com/episen/ing3/tpmra/model/Lock.java
+++ src/main/java/com/episen/ing3/tpmra/model/Lock.java
@@ -17,89 +17,96 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "un verrou")
 @Validated
 public class Lock   {
-  @JsonProperty("owner")
-  private String owner = null;
 
-  @JsonProperty("created")
-  private OffsetDateTime created = null;
+	@JsonProperty("owner")
+	private String documentId = null;
 
-  public Lock owner(String owner) {
-    this.owner = owner;
-    return this;
-  }
+	@JsonProperty("owner")
+	private String owner = null;
 
-  /**
-   * utilisateur propriétaire du verrou
-   * @return owner
-   **/
-  @Schema(description = "utilisateur propriétaire du verrou")
-  
-    public String getOwner() {
-    return owner;
-  }
-
-  public void setOwner(String owner) {
-    this.owner = owner;
-  }
-
-  public Lock created(OffsetDateTime created) {
-    this.created = created;
-    return this;
-  }
-
-  /**
-   * date de la pose du verrou
-   * @return created
-   **/
-  @Schema(description = "date de la pose du verrou")
-  
-    @Valid
-    public OffsetDateTime getCreated() {
-    return created;
-  }
-
-  public void setCreated(OffsetDateTime created) {
-    this.created = created;
-  }
+	@JsonProperty("created")
+	private OffsetDateTime created = null;
 
 
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Lock lock = (Lock) o;
-    return Objects.equals(this.owner, lock.owner) &&
-        Objects.equals(this.created, lock.created);
-  }
+	public Lock(String documentId, String owner, OffsetDateTime created) {
+		super();
+		this.documentId = documentId;
+		this.owner = owner;
+		this.created = created;
+	}
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(owner, created);
-  }
+	/**
+	 * utilisateur propriétaire du verrou
+	 * @return owner
+	 **/
+	@Schema(description = "utilisateur propriétaire du verrou")
 
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class Lock {\n");
-    
-    sb.append("    owner: ").append(toIndentedString(owner)).append("\n");
-    sb.append("    created: ").append(toIndentedString(created)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
+	public String getOwner() {
+		return owner;
+	}
 
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public Lock created(OffsetDateTime created) {
+		this.created = created;
+		return this;
+	}
+
+	/**
+	 * date de la pose du verrou
+	 * @return created
+	 **/
+	@Schema(description = "date de la pose du verrou")
+
+	@Valid
+	public OffsetDateTime getCreated() {
+		return created;
+	}
+
+	public void setCreated(OffsetDateTime created) {
+		this.created = created;
+	}
+
+
+	@Override
+	public boolean equals(java.lang.Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Lock lock = (Lock) o;
+		return Objects.equals(this.owner, lock.owner) &&
+				Objects.equals(this.created, lock.created);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(owner, created);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("class Lock {\n");
+
+		sb.append("    owner: ").append(toIndentedString(owner)).append("\n");
+		sb.append("    created: ").append(toIndentedString(created)).append("\n");
+		sb.append("}");
+		return sb.toString();
+	}
+
+	/**
+	 * Convert the given object to string with each line indented by 4 spaces
+	 * (except the first line).
+	 */
+	private String toIndentedString(java.lang.Object o) {
+		if (o == null) {
+			return "null";
+		}
+		return o.toString().replace("\n", "\n    ");
+	}
 }
