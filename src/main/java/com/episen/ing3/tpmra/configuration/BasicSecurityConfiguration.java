c878b5083066d8e2c74eb4cbe1348b21ab1867d3
Pierre Jacquet <pierre.jacquet@etu.u-pec.fr>
2020-12-05 16:05:19 +0100

    Update BasicSecurityConfiguration

--- src/main/java/com/episen/ing3/tpmra/configuration/BasicSecurityConfiguration.java
+++ src/main/java/com/episen/ing3/tpmra/configuration/BasicSecurityConfiguration.java
@@ -1,35 +1,52 @@
 package com.episen.ing3.tpmra.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+import lombok.RequiredArgsConstructor;
+
+@Configuration(proxyBeanMethods = false)
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class BasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-          .inMemoryAuthentication()
-          .withUser("user")
-            .password("user")
-            .roles("USER")
-            .and()
-          .withUser("admin")
-            .password("admin")
-            .roles("USER", "ADMIN");
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("redacteur").password(passwordEncoder().encode("redacteur"))
+                .roles("REDACTEUR");
+        auth.inMemoryAuthentication()
+                .withUser("relecteur").password(passwordEncoder().encode("relecteur"))
+                .roles("RELECTEUR");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-          .authorizeRequests()
-          .anyRequest()
-          .authenticated()
-          .and()
-          .httpBasic();
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // allow documentation to be embedded in an iframe
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+         //       .antMatchers("/api/**/user").hasRole("USER")
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
